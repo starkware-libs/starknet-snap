2847a5fea5e3c5662b2350ba66954e7f29349895
Carmit Kleinik <carmit@starkware.co>
2022-02-23 13:38:33 +0200

    succeed requiring a config file, get a key from it, requiring module ec and use it to generate a starkKeyPair from passed key

---
+++ config/config.js
@@ -0,0 +1,6 @@
+export const config = {
+  privateKey: '0x024b2426405fbfcb049da6e3028ceb38b36b5816a84bd885c67d322d852fd7c3',
+  publicKey: '0x071dfd0aa3a50c55a1d1d0243c6681bc809726753cb625dc81fa417ee734e574',
+  accountContractAddress: '0x111e1f2ced8fb81a3dfce8f2d2c26474d77095bdb3aa7d52389156bbb88d1db',
+  erc20ContractAddress: '0x01faa92f95a392dfa627319ff79da27f6599c44d239e0dd8b4aa88e2de38edb3'  //deployed Argent ERC20 contract (via starknet cli. cairo v0.7.0)
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+const {config} = require('../config/config');
+const {ec} = require('starknet');
+
+function generateKeyPair() {
+  const privateKey1 = config.privateKey;
+  // const privateKey2 = stark.randomAddress();
+
+  const starkKeyPair = ec.getKeyPair(privateKey1);
+  // const publicKey = ec.getStarkKey(starkKeyPair);
+
+  // return {starkKeyPair, publicKey};
+  return {privateKey1, starkKeyPair};
+}
+
 wallet.registerRpcMessageHandler(async (originString, requestObject) => {
   switch (requestObject.method) {
     case 'hello':
@@ -9,12 +23,15 @@ wallet.registerRpcMessageHandler(async (originString, requestObject) => {
             description:
               'Carmit, This custom confirmation is just for display purposes.',
             textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
-          },
-        ],
+              'But you can edit the snap source code to make it do something, if you want to!'
+          }
+        ]
       });
 
-    case 'createKeyPair':
+    case 'createKeyPair': {
+      let privateKey1;
+      const result = generateKeyPair();
+
       return wallet.request({
         method: 'snap_confirm',
         params: [
@@ -22,9 +39,11 @@ wallet.registerRpcMessageHandler(async (originString, requestObject) => {
             prompt: 'Create a starknet-keypair',
             description:
               "starknet keyPair is... and with it you'll be able to...",
-          },
-        ],
+            textAreaContent: `1: ${result.privateKey1}\nstarkKeyPair: ${result.starkKeyPair}`
+          }
+        ]
       });
+    }
     default:
       throw new Error('Method not found.');
   }
